d8cb20c55460ed2030929b98e4ce9eec59b9d567
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-09-17 11:30:47 -0400

    added basic math question & solution

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Peter Zerbinos
+ * Created on: Sep 2024
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+//clear
+basic.clearScreen()
+basic.pause(1000)
+
+//statement 
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
+
+//clear
+basic.clearScreen()
+basic.pause(1000)
+
+//multiply
+basic.showString('The perimeter would be:' + (5 * 3).toString() + 'cm^2')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
+
+//clear
+basic.clearScreen()
+basic.pause(1000)
+
+//multiply
+basic.showString('The area would be:' + (2 * (5 + 3)).toString())
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
