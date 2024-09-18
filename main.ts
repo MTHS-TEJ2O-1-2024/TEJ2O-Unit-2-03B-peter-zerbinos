f57a635f75e08a5130616e70da2bf84b6857d196
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-09-18 11:23:33 -0400

    cleaned up the code

--- main.ts
+++ main.ts
@@ -16,18 +16,18 @@ basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
 basic.clearScreen()
 basic.pause(1000)
 
-//multiply
+//multiply + smiley
 basic.showString('The perimeter would be:' + (2 * (5 + 3)).toString() + 'cm')
 basic.showIcon(IconNames.Happy)
-basic.pause(100)
-basic.clearScreen()
 
 //clear
 basic.clearScreen()
 basic.pause(1000)
 
-//multiply
+//multiply + smiley
 basic.showString('The area would be:' + (5 * 3).toString() + 'cm^2')
 basic.showIcon(IconNames.Happy)
-basic.pause(100)
+
+//clear
 basic.clearScreen()
+basic.pause(1000)
