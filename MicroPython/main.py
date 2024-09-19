739135c675443b1bd3a44af377d34cd66163a26c
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-09-19 03:47:33 +0000

    created a hex file and edited the MicroPython code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,35 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Copyright (c) 2020 MTHS All rights reserved
+
+Created by: Peter Zerbinos
+Created on: Sep 2024
+This program does basic math
+
 """
 
-from microbit import *
+# clear + pause
+basic.clear_screen()
+basic.pause(1000)
 
+# statement 
+basic.show_string('A rectangle has dimensions 5 cm & 3 cm.')
 
-display.scroll("Hello, World!")
+# clear + pause
+basic.clear_screen()
+basic.pause(1000)
+
+# multiply + smiley
+basic.show_string('The perimeter would be:' + str((2 * (5 + 3))) + 'cm')
+basic.show_icon(IconNames.HAPPY)
+
+# clear + pause
+basic.clear_screen()
+basic.pause(1000)
+
+# multiply + smiley
+basic.show_string('The area would be:' + str((5 * 3)) + 'cm^2')
+basic.show_icon(IconNames.HAPPY)
+
+# clear + pause
+basic.clear_screen()
+basic.pause(1000)
